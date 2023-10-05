9222384b576ecb77d12611f518cf5c12e33128ad
Ganesh Auti <96362269+ganeshauti4466@users.noreply.github.com>
2023-10-05 10:35:42 +0530

    Update Careeropp.jsx

--- Client/src/components/catalog/Careeropp.jsx
+++ Client/src/components/catalog/Careeropp.jsx
@@ -3,7 +3,7 @@ import Link from './Link';
 
 const Careeropp = () => {
     const getUrl = () => {
-        return `https://www.stellantisdigitalhub.com/#/careeroverview`
+        return `careeroverview page link`
     };
     return (
         <div className='ictindia_page' style={{ borderRadius: '21px', backgroundColor: '#f1f1f1', fontSize: '8px', padding: '5px', boxShadow: '2px 2px 4px rgba(150, 149, 149, 0.4)' }}>

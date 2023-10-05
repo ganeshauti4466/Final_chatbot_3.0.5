038eefe2b3aee5e3f56a5d55c606d4559a9dd67a
Ganesh Auti <96362269+ganeshauti4466@users.noreply.github.com>
2023-10-05 10:35:23 +0530

    Update Moreinfo.jsx

--- Client/src/components/catalog/Moreinfo.jsx
+++ Client/src/components/catalog/Moreinfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from './Link'
 const Moreinfo = () => {
     const getUrl = () => {
-        return `https://www.stellantis.com/en`
+        return `info link`
     };
     return (
         <div className='info_page' style={{borderRadius:'21px' , backgroundColor: '#f1f1f1' , fontSize:'8px' , padding: '5px' , boxShadow: '2px 2px 4px rgba(150, 149, 149, 0.4)' }}>

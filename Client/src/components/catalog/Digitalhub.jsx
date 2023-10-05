95afe70dcad2f556559cae06b72421bfe52d4257
Ganesh Auti <96362269+ganeshauti4466@users.noreply.github.com>
2023-10-05 10:34:58 +0530

    Update Digitalhub.jsx

--- Client/src/components/catalog/Digitalhub.jsx
+++ Client/src/components/catalog/Digitalhub.jsx
@@ -3,7 +3,7 @@ import Link from './Link';
 
 const Digitalhub = () => {
     const getUrl = () => {
-        return `https://www.stellantisdigitalhub.com/`
+        return `Your org site link`
     };
     return (
         <div className='ictindia_page' style={{borderRadius:'21px' , backgroundColor: '#f1f1f1' , fontSize:'8px' , padding: '5px' , boxShadow: '2px 2px 4px rgba(150, 149, 149, 0.4)' }}>
